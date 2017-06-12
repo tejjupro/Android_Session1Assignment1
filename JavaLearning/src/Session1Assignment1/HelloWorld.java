5b21e6652045a53f805f8e6b555bed0fba48378d
tejjupro <bonda.tej@gmail.com>
2017-06-12 12:48:45 +0530

    Add files via upload

---
+++ JavaLearning/src/Session1Assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+package Session1Assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	// This is print "Hello World"
+	{
+		
+           System.out.println("Hello World");
+	}
+
+}
